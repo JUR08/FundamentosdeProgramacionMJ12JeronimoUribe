d617225aec5241a760202e4d82e132acdd1bade9
JUR08 <jeronimo.uriber@upb.edu.co>
2026-07-28 13:35:00 -0500

    Mi primer commit

---
+++ 1.VariablesConstantes/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _1.VariablesConstantes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre = "Bob Esponja";
+            Console.WriteLine("Bienvenido" + nombre + " al curso Fundamentos de Programacion");
+        }
+    }
+}
