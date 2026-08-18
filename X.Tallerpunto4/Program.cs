9e1481403f7d0ffd369c31dc63502441c9cb4424
JUR08 <jeronimo.uriber@upb.edu.co>
2026-08-18 13:26:18 -0500

    4to punto taller
    Taller de preparación para el segundo parcial

---
+++ X.Tallerpunto4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace X.Tallerpunto4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pulse la D para que el personaje vaya a la derecha o I para ir a la izquierda");
+            ConsoleKeyInfo tecla = Console.ReadKey();
+            if (tecla.Key == ConsoleKey.D)
+            {
+                Console.WriteLine(": El personaje se mueve a la derecha");
+            }
+            else if (tecla.Key == ConsoleKey.I)
+            {
+                Console.WriteLine(": El personaje se mueve a la izquierda");
+            }
+            else
+            {
+                Console.WriteLine(": No me puedo mover en esa dirección");
+            }
+        }
+    }
+}
