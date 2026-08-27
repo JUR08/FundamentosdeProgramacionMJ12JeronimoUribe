86666b5c61e02ef82de7fb59fe66ce32fe6f22a4
JUR08 <jeronimo.uriber@upb.edu.co>
2026-08-27 13:30:44 -0500

    Ciclo While

---
+++ 10.CicloWhile4/10.CicloWhile4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _10.CicloWhile4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que cuente números pares de rango 1 a n
+            int contador = 1;
+            int num = 0;
+            int par = 0;
+            Console.WriteLine("Ingrese un número");
+            num = int.Parse(Console.ReadLine());
+            while (contador < num)
+            {
+                contador++;
+                    if (contador % 2 == 0)
+                {
+                    par++;
+                }
+            }
+            Console.WriteLine("La cantidad de pares del 1 al " + num + " es de " + par);
+        }
+    }
+}
