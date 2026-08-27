de9537766f01e6989cc890e2dd235c02d9b09ca8
JUR08 <jeronimo.uriber@upb.edu.co>
2026-08-27 12:40:25 -0500

    Ciclo While

---
+++ 8.CicloWhile2/8.CicloWhile2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _8.CicloWhile2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Algoritmo que pida números enteros positivos y los sume, hasta que se ingrese un número entero negativo. Se debe mostrar por pantalla el total de la suma de los números negativos 
+            //Variable bandera: que puede cambiar de estado
+            //Contador: variable numérica que aumenta o disminuye su valor en una cantidad fija (constante) cada vez que ocurre un evento o se repite un proceso
+            //Acumulador: variable numérica que aumenta o disminuye su valor en una cantidad fija (constante) cada vez que ocurre un evento o se repite un proceso
+
+            int sumaEntero = 0; //el acumulador
+            int numero = 0;
+            Console.WriteLine("Ingrese un número para sumar");
+            numero = int.Parse(Console.ReadLine());
+
+            while (numero >= 0) //Esta es la variable bandera
+            {
+                sumaEntero += numero; //Le va sumando a la suma de entero el numero
+                Console.WriteLine("Ingrese un número para sumar");
+                numero = int.Parse(Console.ReadLine()); //Para que se digiten tantos numeros como sean necesarios hasta que aparezca uno negativo
+            }
+            Console.WriteLine("La suma de los números ingresados es: " + sumaEntero);
+        }
+    }
+}
