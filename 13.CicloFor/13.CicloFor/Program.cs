2a447d3697f5dfd2593b6d87c07db9dd0178a96e
JUR08 <jeronimo.uriber@upb.edu.co>
2026-09-03 13:25:51 -0500

    Ciclo For

---
+++ 13.CicloFor/13.CicloFor/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _13.CicloFor
+{
+    internal class Program
+    {
+        static 
+            void Main(string[] args)
+        {
+            /*           int acumulador = 0;
+
+                       for (int contador = 1; contador <= 5; contador++) 
+                       {
+                           acumulador += contador;
+                       }
+                       Console.WriteLine($"La suma de los cinco primeros números enteros es: {acumulador}");*/
+            //Algoritmo que pida numero y calcule factorial
+            Console.WriteLine("Digite un número");
+            int num = int.Parse(Console.ReadLine());
+            int fac = 1;
+        
+            for (int contador = 1; contador <= num; contador++)
+            {
+                fac *= contador;
+            }
+            Console.WriteLine("El factorial de " + (num) + " es " + (fac));
+        }
+    }
+}
