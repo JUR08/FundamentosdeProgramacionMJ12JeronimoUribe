c350d73cc46b521738ba8bbd295f7cfb00cefbe0
JUR08 <jeronimo.uriber@upb.edu.co>
2026-09-08 12:18:03 -0500

    Primer punto taller ciclos

---
+++ Taller2Ciclos1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Taller2Ciclos1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Calcular promedio de calificaciones, debe introducir tantas como desee, detectar cuando no desea continuar
+            int calif = 0;
+            string respuesta;
+            int contcalif = 0;
+            int contbucle = 0;
+            int prom = 0;
+            do
+            {
+                Console.WriteLine("Digite la calificación");
+                calif = int.Parse(Console.ReadLine());
+                Console.WriteLine("Desea continuar? si o no");
+                respuesta = Console.ReadLine();
+                contcalif += calif;
+                contbucle++;
+            } while (respuesta == "si");
+            prom = (contcalif / contbucle);
+            Console.WriteLine("El promedio de notas es de " + (prom));
+        }
+    }
+}
