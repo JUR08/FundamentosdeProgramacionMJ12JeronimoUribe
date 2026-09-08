14b5940ec9d07325646687e98246fd09f8594057
JUR08 <jeronimo.uriber@upb.edu.co>
2026-09-08 13:19:40 -0500

    3er punto del taller de ciclos

---
+++ Taller2Ciclos3/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+namespace Taller2Ciclos3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            Dados dos números enteros ingresados por teclado: b que es la base y
+                          e que es el exponente, se requiere calcular el resultado de la
+                          potenciación.
+                          Ejemplo: b = 2, e = 5  25 = 2 * 2 * 2 * 2 * 2 = 32
+                          Mostrar por pantalla el resultado de la potenciación. 
+                          Seguir pidiendo por teclado la base y el exponente y realizar la
+                          potenciación correspondiente, hasta que el usuario ingrese por teclado
+                          el carácter de escape ‘n’ */
+            string resp;
+            do
+            {
+
+
+                Console.WriteLine("Ingrese una base");
+                int bas = int.Parse(Console.ReadLine());
+                Console.WriteLine("Ingrese un exponente");
+                int exp = int.Parse(Console.ReadLine());
+                int cont = 0;
+                int pot = 1;
+                do
+                {
+                    cont++;
+                    pot *= bas;
+
+                } while (cont < exp);
+                Console.WriteLine(pot);
+                Console.WriteLine("Desea continuar?");
+                Console.WriteLine("Si = s                                      No = n");
+                resp = Console.ReadLine();
+            } while (resp == "s");
+ 
+        }
+    }
+}
---
+++ Taller2Ciclos3/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Taller2Ciclos3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Taller2Ciclos3")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("aac8cb26-5bfa-41e3-a81f-893e3f373a5f")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
